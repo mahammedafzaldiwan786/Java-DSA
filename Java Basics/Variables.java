b38cb0bd2b60e1074cd8407440798343040f5232
mahammedafzal786 <afzalkhaliddiwan2003@gmail.com>
2026-01-10 22:11:25 +0530

    Variables

---
+++ Java Basics/Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main(String[] args) {
+        // Declare and initialize variables
+        int number = 10;
+        String text = "Java Variables";
+
+        // Print the variables to the console
+        System.out.println("Number: " + number);
+        System.out.println("Text: " + text);
+    }
+}
