23d718184dc882bf8cc52dd690421fbbdff24c0d
mahammedafzal786 <afzalkhaliddiwan2003@gmail.com>
2026-01-12 21:43:32 +0530

    practice on datatype, variable & input/output

---
+++ Practice Questions/ProductOfTwoNumbersUserInput.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class ProductOfTwoNumbersUserInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the first number: ");
+        int num1 = sc.nextInt();
+        
+        System.out.print("Enter the second number: ");
+        int num2 = sc.nextInt();
+        
+        int product = num1 * num2;
+        System.out.println("The product of " + num1 + " and " + num2 + " is: " + product);
+    }
+}
