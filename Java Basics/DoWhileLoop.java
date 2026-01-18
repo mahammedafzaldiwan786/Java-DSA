2dfb4bf502430252c67ae89dd589102cc0456902
mahammedafzal786 <afzalkhaliddiwan2003@gmail.com>
2026-01-18 21:17:29 +0530

    While Loop & Do-While Loop

---
+++ Java Basics/DoWhileLoop.java
@@ -0,0 +1,11 @@
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int count = 1;
+        System.out.println("Do-While Loop Example:");
+        // Using do-while loop to print numbers from 1 to 10
+        do {
+            System.out.println("Count: " + count);
+            count++;
+        } while (count <= 10);
+    }
+}
---
+++ Java Basics/WhileLoop.java
@@ -0,0 +1,11 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int count = 1;
+        System.out.println("While Loop Example:");
+        // Using while loop to print numbers from 1 to 5
+        while (count <= 10) {
+            System.out.println("Count: " + count);
+            count++;
+        }
+    }
+}
